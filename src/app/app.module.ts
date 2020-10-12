02139609069a21345fa6df03e24789de3cd68304
Oroden <jimmykasprzak@gmail.com>
2020-10-12 22:43:43 +0200

    :sparkles: start lab 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,15 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
-import {FormsModule} from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {ProductComponent} from './home/product/product.component';
-import {MenuComponent} from './menu/menu.component';
-import {HomeComponent} from './home/home.component';
-import {BasketComponent} from './basket/basket.component';
+import { AppComponent } from './app.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        ProductComponent,
-        MenuComponent,
-        HomeComponent,
-        BasketComponent
     ],
     imports: [
         BrowserModule,
-        AppRoutingModule,
-        HttpClientModule,
-        FormsModule
-    ],
-    providers: [
-        { provide: 'welcomeMsg', useValue: 'Bienvenue sur IMT Ecommerce' }
     ],
     bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule {}
