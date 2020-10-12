68b619dde734c1a1f8e2053cea4653e122bc08d0
Oroden <jimmykasprzak@gmail.com>
2020-10-13 00:03:31 +0200

    :sparkles: correction lab 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
     declarations: [
         AppComponent,
+        MenuComponent,
+        FooterComponent,
     ],
     imports: [
         BrowserModule,
