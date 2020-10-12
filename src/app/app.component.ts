52fe6ef6f1f2fd487b7bf07e8401e63ba10a7ae2
Oroden <jimmykasprzak@gmail.com>
2020-10-12 22:45:27 +0200

    :sparkles: start lab 1 without title defined

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Bienvenue sur IMT Ecommerce';
-}
+export class AppComponent {}
