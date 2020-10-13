834d394d900e708e728b8c95604c740da8b0b489
Oroden <jimmykasprzak@gmail.com>
2020-10-13 17:16:42 +0200

    :sparkles: correction lab 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { FooterComponent } from './footer/footer.component';
 import { ProductComponent } from './product/product.component';
+
 
 @NgModule({
     declarations: [
@@ -15,6 +19,9 @@ import { ProductComponent } from './product/product.component';
     ],
     imports: [
         BrowserModule,
+        BrowserAnimationsModule,
+        MatButtonModule,
+        MatCardModule,
     ],
     bootstrap: [AppComponent]
 })
